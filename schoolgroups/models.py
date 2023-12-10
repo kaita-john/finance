0dd349017765988446c71ae640993786217bd135
kaita <johnkaitadocuments@gmail.com>
2023-12-10 19:28:26 +0300

    Removed school_id from SchoolGroup Model

--- schoolgroups/models.py
+++ schoolgroups/models.py
@@ -6,7 +6,7 @@ from models import ParentModel
 # Create your models here.
 class SchoolGroup(ParentModel):
     name = models.CharField(max_length=255)
-    school_id = models.UUIDField(max_length=255)
+    school_id = models.UUIDField(max_length=255, null=True, blank=True)
     def __str__(self):
         return self.name
 
