8c8d2dfb2be8d34c8ec77db3ac3ee3fa0e23998b
dennis.kirimi <denniskirimi.dk@gmail.com>
2024-01-26 15:58:46 +0300

    K

--- term/serializers.py
+++ term/serializers.py
@@ -4,11 +4,6 @@ from term.models import Term
 
 
 class TermSerializer(serializers.ModelSerializer):
-    academic_year_id = serializers.SerializerMethodField()
-
-    def get_academic_year_id(self, obj):
-        return str(obj.academic_year.id) if obj.academic_year else None
-
     class Meta:
-            model = Term
-            fields = '__all__'
+        model = Term
+        fields = '__all__'
