60f7745f82947ec1e21772d78ebc68c3ed902a17
dennis.kirimi <denniskirimi.dk@gmail.com>
2024-01-26 12:01:06 +0300

    K

--- invoices/views.py
+++ invoices/views.py
@@ -169,7 +169,7 @@ def createInvoices(school_id, students, structure_year, structure_term, structur
                         exists_query = Invoice.objects.filter(school_id=school_id, votehead__id=votehead.id, term=term, year=year, student=student)
 
                         if exists_query.exists():
-                            print(f"Stopped and Student is {student} and Fee structure Class is {classes} and student class is {student.classes} and Votehead is {votehead.vote_head_name}")
+                            print(f"Stopped and Student is {student} and Fee structure Class is {classes} and student class is {student.current_Class} and Votehead is {votehead.vote_head_name}")
 
                             print("Invoice already exists there!")
                             invoice = exists_query[0]
