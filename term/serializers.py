a6f86334439000a45491bb35b4442e2c1b07bf2c
dennis.kirimi <denniskirimi.dk@gmail.com>
2024-01-26 15:45:11 +0300

    K

--- term/serializers.py
+++ term/serializers.py
@@ -4,6 +4,8 @@ from term.models import Term
 
 
 class TermSerializer(serializers.ModelSerializer):
+    academic_year_id = serializers.UUIDField(source='academic_year.id', read_only=True)
+
     class Meta:
         model = Term
         fields = '__all__'
