55e417a97a1510ea97e3d7aacbb8dfc75a209cce
dennis.kirimi <denniskirimi.dk@gmail.com>
2024-01-26 09:21:09 +0300

    K

--- invoices/views.py
+++ invoices/views.py
@@ -417,7 +417,7 @@ class invoiceView(SchoolIdMixin, generics.GenericAPIView):
 
         fullList = []
 
-        terms_list = Term.objects.filter(school_id=school_id, academic_year = academic_year)
+        terms_list = Term.objects.filter(school_id=school_id)
 
         print("Here")
         for term in terms_list:
