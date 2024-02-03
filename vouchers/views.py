952fa953dc236fb98e9a3327f7774fcb7114e349
kaita <johnkaitadocuments@gmail.com>
2024-02-03 16:12:32 +0300

    Many changes

--- vouchers/views.py
+++ vouchers/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 
 from financial_years.models import FinancialYear
 from utils import SchoolIdMixin, IsAdminOrSuperUser, UUID_from_PrimaryKey, DefaultMixin
+from voucher_attachments.models import VoucherAttachment
 from voucher_attachments.serializers import Voucherattachmentserializer
 from voucher_items.models import VoucherItem
 from voucher_items.serializers import VoucherItemSerializer
@@ -151,22 +152,45 @@ class VoucherDetailView(SchoolIdMixin, DefaultMixin, generics.RetrieveUpdateDest
             try:
                 with transaction.atomic():
                     VoucherItem.objects.filter(voucher=instance).delete()
+                    VoucherAttachment.objects.filter(voucher=instance).delete()
 
-                    payment_items_data = serializer.validated_data.get('payment_values', [])
+                    current_financial_year = FinancialYear.objects.get(is_current=True, school=school_id)
+                    serializer.validated_data['financial_year'] = current_financial_year
                     voucher = serializer.save()
+
+                    bank_account = voucher.bank_account
+                    initial_voucher_amount = instance.totalAmount
+                    initial_balance = bank_account.balance
+                    if initial_voucher_amount is not None:
+                        initial_balance += Decimal(initial_voucher_amount)
+                    new_voucher_amount = voucher.totalAmount
+                    new_balance = initial_balance - Decimal(new_voucher_amount)
+                    bank_account.balance = new_balance
+                    bank_account.save()
+
+                    # Save payment items
+                    payment_items_data = serializer.validated_data.get('payment_values', [])
                     for payment_item_data in payment_items_data:
-                        payment_item_data['school_id'] = voucher.school_id
+                        payment_item_data['school_id'] = school_id
                         payment_item_data['voucher'] = voucher.id
                         payment_item_serializer = VoucherItemSerializer(data=payment_item_data)
                         payment_item_serializer.is_valid(raise_exception=True)
                         payment_item_serializer.save()
 
-                return Response({'detail': 'Voucher updated successfully'}, status=status.HTTP_200_OK)
+                    # Save attachments
+                    attachments_data = serializer.validated_data.get('attachments_values', [])
+                    for attachment_data in attachments_data:
+                        attachment_data['school_id'] = school_id
+                        attachment_data['voucher'] = voucher.id
+                        attachment_serializer = Voucherattachmentserializer(data=attachment_data)
+                        attachment_serializer.is_valid(raise_exception=True)
+                        attachment_serializer.save()
+
+                    return Response({'detail': 'Voucher updated successfully'}, status=status.HTTP_200_OK)
             except Exception as e:
                 return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
 
     def perform_update(self, serializer):
         serializer.save()
