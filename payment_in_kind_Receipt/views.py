e0b31e60ae8e3829cfe619f826c95fa746beed27
kaita <johnkaitadocuments@gmail.com>
2024-02-15 12:08:42 +0300

    Many changes

--- payment_in_kind_Receipt/views.py
+++ payment_in_kind_Receipt/views.py
@@ -121,6 +121,7 @@ class PIKReceiptCreateView(SchoolIdMixin, DefaultMixin, generics.CreateAPIView):
                             raise ValueError(f"Amount entered is more than required balance for votehead {invoice_instance.votehead.vote_head_name}")
 
                     except Invoice.DoesNotExist:
+                        print(f"Didn't find invoice so creating overpayment")
                         overpayment += created_Pik.amount
                     except Invoice.MultipleObjectsReturned:
                         raise ValueError("Transaction cancelled: Multiple invoices found for the given criteria")
