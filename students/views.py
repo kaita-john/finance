ef5197d99a4f1e98651ad74646d2d69b32caad47
kaita <johnkaitadocuments@gmail.com>
2024-07-13 08:06:40 +0300

    Added Pagination

--- students/views.py
+++ students/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import generics, status
 from rest_framework.exceptions import NotFound
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
